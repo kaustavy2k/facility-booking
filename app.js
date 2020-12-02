c2c82c72b1e536cf9e1df6a8d61cb4167ca3a854
kaustavy2k <kaustavbasaky2k@gmail.com>
2020-12-02 19:39:52 +0530

    heroku chnages

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const cookieParser=require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const getFacilities = require("./routes/getFacilities");
 const cors = require("cors");
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
-app.use("/", getFacilities);
 
+app.use("/", getFacilities);
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
 module.exports = app;
